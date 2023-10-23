82cb19a3b648d0ecbf595b1c8a9caf420f4cdeb7
Chandrakanth0698 <chandrakanth0698@gmail.com>
2023-10-23 15:17:16 -0400

    Todo list app using Command line interface

---
+++ Todo_cli.py
@@ -0,0 +1,20 @@
+user_prompt = "Enter a todo: add, show, remove -> "
+todo_list = []
+
+while True:
+    todo = input(user_prompt)
+    cmd, task = todo.split(" ")[0], " ".join(todo.split(" ")[1:])
+    if cmd == "add":
+        todo_list.append(task)
+    elif cmd == "show":
+        for i in range(len(todo_list)):
+            print(i+1, " ", todo_list[i])
+    elif cmd == "remove":
+        num = int(input("enter task number: "))
+        if num > len(todo_list):
+            print("enter valid number")
+            continue
+        else:
+            todo_list.pop(num-1)
+    else:
+        print("enter a valid Input: eg: add <task> or show or remove ")
