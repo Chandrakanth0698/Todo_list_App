459ff47ed9d58302c9218ead68f36fd7aec17739
Chandrakanth0698 <chandrakanth0698@gmail.com>
2023-10-30 11:30:48 -0400

    Implement Add feature

--- web_app.py
+++ web_app.py
@@ -6,10 +6,20 @@ if not os.path.exists('todo.txt'):
     with open('todo.txt', 'w') as file:
         pass
 
+
+def add_todo():
+    task = st.session_state["new_todo"]
+    todo_list.append(task+"\n")
+    functions.write_todo_file(todo_list)
+
+
 todo_list = functions.read_todo_file()
 st.title("My Todo App")
 st.subheader("Add your daily tasks")
-st.checkbox("to add new todo")
 
-task = st.text_input("", placeholder="Add new todo...")
+for index, todo in enumerate(todo_list):
+    st.checkbox(todo,key=index)
+
+st.text_input("", placeholder="Add new todo...", on_change=add_todo, key='new_todo')
+st.session_state
 
